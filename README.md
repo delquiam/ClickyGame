6bc8b2dcb3f99536e7b8278befc7bbd55f6173ec
wonder woman <rdelquiam@gmail.com>
2019-03-02 21:54:46 -0800

    added read me

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ClickyGame
+
+Sanrio Character Clicker Game is a memory game that was built using React, Bootstrap and ES6.  The application's UI is broken into 4 components.  The object of the game is to not click on the same character twice.
+
+![image](clicky-game/src/images/sanrio.png)
--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -6,114 +6,72 @@ import Wrapper from "./components/Wrapper";
 import characters from "./characters.json";
 
 class App extends Component {
-  
-    state = {
-      characters,
-      clickedCharacters: [],
-      score: 0
-    };
-  
-  //when you click on a card ... the fish is taken out of the array
-    imageClick = event => {
-      const currentCharacters = event.target.alt;
-      const charactersAlreadyClicked =
-        this.state.clickedCharacters.indexOf(currentCharacters) > -1;
-  
-  //if you click on a fish that has already been selected, the game is reset and cards reordered
-      if (charactersAlreadyClicked) {
-        this.setState({
+  state = {
+    characters,
+    clickedCharacters: [],
+    score: 0
+  };
+  //when you click on a card , the character is taken out of the array
+  imageClick = event => {
+    const currentCharacters = event.target.alt;
+    const charactersAlreadyClicked =
+      this.state.clickedCharacters.indexOf(currentCharacters) > -1;
+
+    //if you click on a character that has already been selected, the game is reset and cards reshuffled
+    if (charactersAlreadyClicked) {
+      this.setState({
+        characters: this.state.characters.sort(function(a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedCharacters: [],
+        score: 0
+      });
+      alert("Sorry, you lost");
+      //if you click on an available character, your score is increased and cards reshuffled
+    } else {
+      this.setState(
+        {
           characters: this.state.characters.sort(function(a, b) {
             return 0.5 - Math.random();
           }),
-          clickedCharacters: [],
-          score: 0
-        });
-          alert("Sorry, you lost");
-  
-  //if you click on an available fish, your score is increased and cards reordered
-      } else {
-        this.setState(
-          {
-            characters: this.state.characters.sort(function(a, b) {
-              return 0.5 - Math.random();
-            }),
-            clickedCharacters: this.state.clickedCharacters.concat(
-              currentCharacters
-            ),
-            score: this.state.score + 1
-          },
-  //if you get all 12 fish corrent you get a congrats message and the game resets        
-          () => {
-            if (this.state.score === 12) {
-              alert("Yay! You Win!");
-              this.setState({
-                characters: this.state.characters.sort(function(a, b) {
-                  return 0.5 - Math.random();
-                }),
-                clickedCharacters: [],
-                score: 0
-              });
-            }
+          clickedCharacters: this.state.clickedCharacters.concat(
+            currentCharacters
+          ),
+          score: this.state.score + 1
+        },
+        //if you get all 12 characters correct you get an alert and the game resets
+        () => {
+          if (this.state.score === 12) {
+            alert("Yay! You Win!");
+            this.setState({
+              characters: this.state.characters.sort(function(a, b) {
+                return 0.5 - Math.random();
+              }),
+              clickedCharacters: [],
+              score: 0
+            });
           }
-        );
-      }
-    };
-  
-  //the order of components to be rendered: navbar, jumbotron, friendcard, footer 
-    render() {
-      return (
-        <>
-               <Wrapper>
-               
-                 <Jumbotron />
-              
-
-                <Navbar score={this.state.score} />
-                {this.state.characters.map(character => (
+        }
+      );
+    }
+  };
+  render() {
+    return (
+      <>
+        <Wrapper>
+          <Jumbotron />
+          <Navbar score={this.state.score} />
+          {this.state.characters.map(character => (
             <CharacterCard
-            imageClick={this.imageClick}
-            id={character.id}
-            key={character.id}
-            image={character.image}
-            />))}
-      
-      </Wrapper>
+              imageClick={this.imageClick}
+              id={character.id}
+              key={character.id}
+              image={character.image}
+            />
+          ))}
+        </Wrapper>
       </>
     );
   }
 }
-  export default App;
-//   state = {
-//     characters,
-//     score: 0,
-//     highScore: 0,
-//     guessed: []
-//   };
-
-  
-//   render() {
-//     return (
-//       <>
-//       <Wrapper>
-//         <div className="row">
-//           <Jumbotron />
-//         </div>
-
-//         <Navbar score={this.state.score} highScore={this.state.highScore} />
-//         <div className="row">
-//           {this.state.characters.map(character => (
-//             <CharacterCard
-//               key={character.id}
-//               id={character.id}
-//               name={character.name}
-//               image={character.image}
-//               updateScore={this.updateScore}
-//             />))}
-//         </div>
-//       </Wrapper>
-//       </>
-//     );
-//   }
-// }
-
-// export default App;
+export default App;
