820d79bc540907c8be5f15679a792c06c79a695b
wonder woman <rdelquiam@gmail.com>
2019-03-05 18:04:30 -0800

    Updates

---
+++ precache-manifest.b0dca0d57ef79c3f0686399b9703da5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "38804bafd5e116940d15c3a01539a489",
+    "url": "/ClickyGame/static/media/wishmemell.38804baf.png"
+  },
+  {
+    "revision": "80dac6d90ea4d0e703e1e3f06853ed3e",
+    "url": "/ClickyGame/static/media/sanrio.80dac6d9.png"
+  },
+  {
+    "revision": "e02dcfb3f1415e0685f33da4154efc84",
+    "url": "/ClickyGame/static/media/bonbonribbon.e02dcfb3.png"
+  },
+  {
+    "revision": "2c3a1b5a67c5d677a07d",
+    "url": "/ClickyGame/static/js/runtime~main.2c3a1b5a.js"
+  },
+  {
+    "revision": "5483a26656609f116ee8",
+    "url": "/ClickyGame/static/js/main.5483a266.chunk.js"
+  },
+  {
+    "revision": "62507dd0e61ef6a60d15",
+    "url": "/ClickyGame/static/js/2.62507dd0.chunk.js"
+  },
+  {
+    "revision": "5483a26656609f116ee8",
+    "url": "/ClickyGame/static/css/main.f26f87b7.chunk.css"
+  },
+  {
+    "revision": "d1e4b7f35f3152e3164faf5953eecd83",
+    "url": "/ClickyGame/index.html"
+  }
+];
